99ddc9e911053f1788996141a90a71ae0dd9e855
Cassidy Harootunian <cassidyhbusiness.20@gmail.com>
2026-03-25 19:20:04 -0700

    Add Cloudflare domains to CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -18,6 +18,9 @@ origins = [
     "http://127.0.0.1:3001",
     "http://localhost:5500",
     "http://127.0.0.1:5500",
+    "https://quant-research-critic.pages.dev",
+    "https://quantcritic.com",
+    "https://www.quantcritic.com",
 ]
 
 app.add_middleware(
